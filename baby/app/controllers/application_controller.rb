9f86f8ce330da8dc6073a3baf8fb503332f49c48
Peter Wang <happypeter1983@gmail.com>
2013-11-25 22:38:39 +0800

    add baby

---
+++ baby/app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+
+  def current_user
+    @current_user ||= User.find_by_token(cookies[:token]) if cookies[:token]
+  end
+
+  helper_method :current_user
+end
---
+++ baby/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baby::Application.config.secret_token = 'cc3f43c4dd0820ad207c4ccae2b9f28c76e59e7e56d39c5a9ff3723afb73aa9d676d091f5c90b8cd2fd9acd2cf1bd87e01e3caae81b9dc009725284a51c1c13d'
